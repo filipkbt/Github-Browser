13846323c088a0741d4717f76aaa33c1241f302c
Filip Kubat <filip.kubat@wp.pl>
2020-02-24 21:12:08 +0100

    Add Angular Material, Add basic firebase config

--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
 
+const angularMaterialModules = [
+  MatInputModule,
+  MatFormFieldModule,
+  MatAutocompleteModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatSnackBarModule,
+  MatCardModule
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    angularMaterialModules
+  ], exports: [
+    angularMaterialModules
   ]
 })
 export class AngularMaterialModule { }
